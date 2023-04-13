dd779a97e7c32d46354fbbce25e2c36c53f49091
Gabo <gabo5612@gmail.com>
2023-04-13 12:41:25 -0400

    Initialize project using Create React App

---
+++ Components/JobListing.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "../Style/JobListing.css"
+
+function JobListing(){
+    return(
+        <React.Fragment/>
+    )
+}
+export {JobListing} 
---
+++ Components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "../Style/SearchBar.css"
+
+function SearchBar(){
+    return(
+        <React.Fragment/>
+    )
+}
+export {SearchBar} 
